61135b738f38c48c01edfbd8d088aa7a45e8503a
cirdeirf <yrr+work@pm.me>
2019-06-05 10:38:03 +0200

    Implement basic mpt algorithm, add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,26 @@
-mod pfa;
+#[macro_use]
 mod pta;
 
 use log_domain::LogDomain;
-use nalgebra::{DMatrix, DVector, RowDVector};
-use pfa::PFA;
-use pta::Tree;
 use pta::PTA;
 
-macro_rules! hashmap {
-    ($( $key: expr => $val: expr ),*) => {{
-         let mut map = ::std::collections::HashMap::new();
-         $( map.insert($key, $val); )*
-         map
-    }}
-}
-
 fn main() {
-    let pfa = PFA::new(
-        RowDVector::from_vec(vec![1., 0., 0.]),
-        DVector::from_vec(vec![0., 0.0, 1.0]),
-        hashmap![
-            'a' =>
-            DMatrix::from_vec(3, 3, vec![0., 0., 0., 0.3, 0., 0., 0., 0.2, 0.]),
-            'b' =>
-            DMatrix::from_vec(3, 3, vec![0., 0., 0., 0.7, 0., 0., 0., 0.8, 0.])],
-    );
-    // println!("m_a: {}", pfa.transitions.get(&'a').unwrap());
-    // println!("m_b: {}", pfa.transitions.get(&'b').unwrap());
-    // println!("alg: {:?}", pfa.most_probable_string());
-
     let pta = PTA::new(
-        hashmap!['a' => 0, 'b' => 0, 's' => 2],
+        hashmap!['a' => 0, 'g' => 1, 's' => 2],
         vec![
+            LogDomain::new(0.0).unwrap(),
+            LogDomain::new(0.0).unwrap(),
             LogDomain::new(1.0).unwrap(),
-            LogDomain::new(0.0).unwrap(),
-            LogDomain::new(0.0).unwrap(),
         ],
         hashmap![
-        'a' => hashmap![1 => vec![(1, 'a', vec![], LogDomain::new(0.5).unwrap())],
-                        2 => vec![(1, 'a', vec![], LogDomain::new(0.4).unwrap())]],
-        'b' => hashmap![1 => vec![(1, 'b', vec![], LogDomain::new(0.2).unwrap())],
-                        2 => vec![(1, 'b', vec![], LogDomain::new(0.6).unwrap())]],
-        's' => hashmap![0 => vec![(0, 's', vec![1, 1], LogDomain::new(0.9).unwrap()),
-                                  (0, 's', vec![2, 2], LogDomain::new(0.1).unwrap())],
-                        1 => vec![(1, 's', vec![1, 2], LogDomain::new(0.3).unwrap())]]],
+        'a' => hashmap![0 => vec![(0, 'a', vec![], LogDomain::new(0.8).unwrap())],
+                        1 => vec![(1, 'a', vec![], LogDomain::new(0.6).unwrap())]],
+        'g' => hashmap![0 => vec![(0, 'g', vec![0], LogDomain::new(0.2).unwrap())],
+                        1 => vec![(0, 'g', vec![1], LogDomain::new(0.1).unwrap())],
+                        1 => vec![(1, 'g', vec![1], LogDomain::new(0.3).unwrap())]],
+        's' => hashmap![2 => vec![(2, 's', vec![1, 1], LogDomain::new(0.9).unwrap()),
+                                  (2, 's', vec![1, 2], LogDomain::new(0.1).unwrap())]]],
     );
-    let xi = Tree {
-        root: 's',
-        children: vec![
-            Tree {
-                root: 'a',
-                children: vec![],
-            },
-            Tree {
-                root: 's',
-                children: vec![],
-            },
-        ],
-    };
-    println!("pp: \t{}", pta.prefix_probability(&xi));
-    println!("p: \t{}", pta.probability(&xi));
+    let mpt = pta.most_probable_tree();
+    println!("{}\t{}", mpt.1, mpt.0);
 }
--- src/pta.rs
+++ src/pta.rs
@@ -1,16 +1,68 @@
 use log_domain::LogDomain;
 use num_traits::Zero;
+use priority_queue::PriorityQueue;
 use std::collections::HashMap;
+use std::fmt;
+
+macro_rules! hashmap {
+    ($( $key: expr => $val: expr ),*) => {{
+         let mut map = ::std::collections::HashMap::new();
+         $( map.insert($key, $val); )*
+         map
+    }}
+}
 
 type TransitionMap = HashMap<
     char,
     HashMap<usize, Vec<(usize, char, Vec<usize>, LogDomain<f64>)>>,
 >;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Tree {
     pub root: char,
     pub children: Vec<Tree>,
+}
+
+impl Tree {
+    pub fn extend(&mut self, s: char, sigma: &HashMap<char, usize>) {
+        let mut t_stack = Vec::new();
+        t_stack.push(self);
+        while !t_stack.is_empty() {
+            let t = t_stack.pop().unwrap();
+            if t.children.len() < sigma[&t.root] {
+                t.children.push(Tree {
+                    root: s,
+                    children: Vec::new(),
+                });
+                break;
+            } else {
+                for t_i in &mut t.children {
+                    t_stack.push(t_i);
+                }
+            }
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut ret = self.root.to_string();
+        if !self.children.is_empty() {
+            ret.push_str("( ");
+            for t_i in &self.children {
+                ret.push_str(&t_i.to_string());
+                ret.push_str(", ");
+            }
+            ret.pop();
+            ret.pop();
+            ret.push_str(" )");
+        }
+        ret
+    }
+}
+
+impl fmt::Display for Tree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
 }
 
 pub struct PTA {
@@ -42,12 +94,16 @@ impl PTA {
         self.rec_probability_root(xi, true)
     }
 
+    fn potential_probability(&self, xi: &Tree) -> LogDomain<f64> {
+        self.prefix_probability(xi)
+    }
+
     fn rec_probability_root(&self, xi: &Tree, prefix: bool) -> LogDomain<f64> {
         self.rec_probability(xi, prefix)
             .iter()
             .zip(&self.root_weights)
             .map(|(&p_q, &root_q)| p_q * root_q)
-            .fold(LogDomain::zero(), |acc, x| acc + x)
+            .sum()
     }
 
     fn rec_probability(&self, xi: &Tree, prefix: bool) -> Vec<LogDomain<f64>> {
@@ -83,8 +139,168 @@ impl PTA {
         ret
     }
 
-    // pub fn most_probable_tree(&self) -> Tree {
-    //     let mut current_prop = LogDomain::zero();
-    //     let mut current_best =
-    // }
+    pub fn most_probable_tree(&self) -> (Tree, LogDomain<f64>) {
+        let mut current_prop = LogDomain::zero();
+        let mut current_best;
+
+        let mut q = PriorityQueue::new();
+        for (s, _) in &self.sigma {
+            let t = Tree {
+                root: s.clone(),
+                children: Vec::new(),
+            };
+            let pp = self.potential_probability(&t);
+            q.push(t, pp);
+        }
+
+        current_best = q.peek().unwrap().0.clone();
+
+        while !q.is_empty() {
+            println!("[");
+            for (t, pp) in &q {
+                println!("\t({} \t {})", pp, t);
+            }
+            println!("]");
+
+            let (t, pp) = q.pop().unwrap();
+            println!("PP({}) \t {}", t, pp);
+
+            if pp > current_prop {
+                let p = self.probability(&t);
+                println!("P({}) \t {}", t, p);
+                if p > current_prop {
+                    current_prop = p;
+                    current_best = t.clone();
+                }
+
+                println!("\nt: {}", t);
+                for (s, _) in &self.sigma {
+                    let mut t_s = t.clone();
+                    t_s.extend(*s, &self.sigma);
+                    // println!("t_s: {}", t_s);
+                    let pp_t_s = self.potential_probability(&t_s);
+                    if pp_t_s > current_prop {
+                        q.push(t_s, pp_t_s);
+                    }
+                }
+            } else {
+                return (current_best, current_prop);
+            }
+        }
+        return (current_best, current_prop);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_most_probable_tree() {
+        let pta = PTA::new(
+            hashmap!['a' => 0, 'b' => 0, 's' => 2],
+            vec![
+                LogDomain::new(0.7).unwrap(),
+                LogDomain::new(0.2).unwrap(),
+                LogDomain::new(0.1).unwrap(),
+            ],
+            hashmap![
+        'a' => hashmap![1 => vec![(1, 'a', vec![], LogDomain::new(0.5).unwrap())],
+                        2 => vec![(2, 'a', vec![], LogDomain::new(0.4).unwrap())]],
+        'b' => hashmap![1 => vec![(1, 'b', vec![], LogDomain::new(0.2).unwrap())],
+                        2 => vec![(2, 'b', vec![], LogDomain::new(0.6).unwrap())]],
+        's' => hashmap![0 => vec![(0, 's', vec![1, 1], LogDomain::new(0.9).unwrap()),
+                                  (0, 's', vec![2, 2], LogDomain::new(0.1).unwrap())],
+                        1 => vec![(1, 's', vec![1, 2], LogDomain::new(0.3).unwrap())]]],
+        );
+        let mpt = pta.most_probable_tree();
+        assert_eq!(
+            mpt.0,
+            Tree {
+                root: 's',
+                children: vec![
+                    Tree {
+                        root: 'a',
+                        children: vec![]
+                    },
+                    Tree {
+                        root: 'a',
+                        children: vec![]
+                    }
+                ]
+            }
+        );
+        assert_eq!(mpt.1, LogDomain::new(0.1807).unwrap());
+    }
+
+    #[test]
+    fn test_probability() {
+        let pta = PTA::new(
+            hashmap!['a' => 0, 'b' => 0, 's' => 2],
+            vec![
+                LogDomain::new(0.7).unwrap(),
+                LogDomain::new(0.2).unwrap(),
+                LogDomain::new(0.1).unwrap(),
+            ],
+            hashmap![
+        'a' => hashmap![1 => vec![(1, 'a', vec![], LogDomain::new(0.5).unwrap())],
+                        2 => vec![(2, 'a', vec![], LogDomain::new(0.4).unwrap())]],
+        'b' => hashmap![1 => vec![(1, 'b', vec![], LogDomain::new(0.2).unwrap())],
+                        2 => vec![(2, 'b', vec![], LogDomain::new(0.6).unwrap())]],
+        's' => hashmap![0 => vec![(0, 's', vec![1, 1], LogDomain::new(0.9).unwrap()),
+                                  (0, 's', vec![2, 2], LogDomain::new(0.1).unwrap())],
+                        1 => vec![(1, 's', vec![1, 2], LogDomain::new(0.3).unwrap())]]],
+        );
+        let xi = Tree {
+            root: 's',
+            children: vec![
+                Tree {
+                    root: 'a',
+                    children: vec![],
+                },
+                Tree {
+                    root: 'b',
+                    children: vec![],
+                },
+            ],
+        };
+        assert_eq!(pta.probability(&xi), LogDomain::new(0.0978).unwrap());
+    }
+
+    #[test]
+    fn test_potential_probability() {
+        let pta = PTA::new(
+            hashmap!['a' => 0, 'b' => 0, 's' => 2],
+            vec![
+                LogDomain::new(0.7).unwrap(),
+                LogDomain::new(0.2).unwrap(),
+                LogDomain::new(0.1).unwrap(),
+            ],
+            hashmap![
+        'a' => hashmap![1 => vec![(1, 'a', vec![], LogDomain::new(0.5).unwrap())],
+                        2 => vec![(2, 'a', vec![], LogDomain::new(0.4).unwrap())]],
+        'b' => hashmap![1 => vec![(1, 'b', vec![], LogDomain::new(0.2).unwrap())],
+                        2 => vec![(2, 'b', vec![], LogDomain::new(0.6).unwrap())]],
+        's' => hashmap![0 => vec![(0, 's', vec![1, 1], LogDomain::new(0.9).unwrap()),
+                                  (0, 's', vec![2, 2], LogDomain::new(0.1).unwrap())],
+                        1 => vec![(1, 's', vec![1, 2], LogDomain::new(0.3).unwrap())]]],
+        );
+        let xi = Tree {
+            root: 's',
+            children: vec![
+                Tree {
+                    root: 'a',
+                    children: vec![],
+                },
+                Tree {
+                    root: 's',
+                    children: vec![],
+                },
+            ],
+        };
+        assert_eq!(
+            pta.potential_probability(&xi),
+            LogDomain::new(0.0945).unwrap()
+        );
+    }
 }
