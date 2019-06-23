1cec64076b8b9133f676189acf27a2b82610b6cf
cirdeirf <yrr+work@pm.me>
2019-06-23 17:51:52 +0200

    Move pta to separate files, basic clap integration

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,57 @@
 mod pta;
 
+use clap::{App, Arg};
 use pta::PTA;
-// use std::collections::HashSet;
+use std::fs;
 use std::time::Instant;
 
 fn main() {
-    let pta_string = "root: 0 # 0.7\n\
-                      root: 1 # 0.2\n\
-                      root: 2 # 0.1\n\
-                      transition: 1 -> a() # 0.5\n\
-                      transition: 2 -> a() # 0.4\n\
-                      transition: 1 -> b() # 0.2\n\
-                      transition: 2 -> b() # 0.6\n\
-                      transition: 0 -> s(1, 1) # 0.9\n\
-                      transition: 0 -> s(2, 2) # 0.1\n\
-                      transition: 1 -> s(1, 2) # 0.3";
-    let pta: PTA<usize, char> = pta_string.parse().unwrap();
-    println!("{}", pta);
+    let matches = App::new("mpt")
+        .version("0.1")
+        .author("Pius Meinert <yrr+work@pm.me>")
+        .about(
+            "Most probable tree and best parse algorithms for probabilistic \
+             tree automata (pta).\n\
+             Implementation for my master's thesis: “The Problem of Computing \
+             the Most Probable Tree of a Probabilistic Tree Automaton”.",
+        )
+        .arg(
+            Arg::with_name("INPUT")
+                .default_value("experiments/default.pta")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .help("Print the PTA (root weight vector and transitions)"),
+        )
+        .arg(
+            Arg::with_name("best_parse")
+                .short("b")
+                .long("best-parse")
+                .help("foo"),
+        )
+        .get_matches();
+
+    let input = fs::read_to_string(matches.value_of("INPUT").unwrap())
+        .expect("Could not read input file");
+    let pta: PTA<String, String> = input.parse().unwrap();
+
+    if matches.is_present("verbose") {
+        println!("{}", pta);
+    }
 
     let start_time = Instant::now();
-    let mpt = pta.most_probable_tree();
-    println!("{}\t{}", mpt.1, mpt.0);
+
+    if matches.is_present("best_parse") {
+        let best_parse = pta.best_parse();
+        println!("{}\t{}", best_parse.1, best_parse.0);
+    } else {
+        let mpt = pta.most_probable_tree();
+        println!("{}\t{}", mpt.1, mpt.0);
+    }
+
     println!("time: {:?}", start_time.elapsed());
-
-    println!("\nbest_parse");
-    let best_parse = pta.best_parse();
-    println!("{}\t{}", best_parse.1, best_parse.0);
-    println!("\n");
-
-    // prime automaton for Psi = {2, 3}
-    let pta_string = "root: 0 # 1.0\n\
-                      transition: 2 -> a() # 0.109\n\
-                      transition: 5 -> a() # 0.109\n\
-                      transition: 0 -> g(1) # 0.5\n\
-                      transition: 0 -> g(3) # 0.5\n\
-                      transition: 1 -> g(2) # 1.0\n\
-                      transition: 2 -> g(1) # 0.891\n\
-                      transition: 3 -> g(4) # 1.0\n\
-                      transition: 4 -> g(5) # 1.0\n\
-                      transition: 5 -> g(3) # 0.891";
-    let pta: PTA<usize, char> = pta_string.parse().unwrap();
-    println!("{}", pta);
-
-    let start_time = Instant::now();
-    let mpt = pta.most_probable_tree();
-    println!("{}\t{}", mpt.1, mpt.0);
-    println!("time: {:?}", start_time.elapsed());
-
-    println!("\nbest_parse");
-    let best_parse = pta.best_parse();
-    println!("{}\t{}", best_parse.1, best_parse.0);
-
-    // let mut xi = "(s (b) (a))".parse().unwrap();
-    // println!(
-    //     "{}\t{}",
-    //     pta.prefix_probability(
-    //         &mut xi,
-    //         &mut HashSet::new(),
-    //         &mut HashSet::new()
-    //     ),
-    //     xi
-    // );
 }
